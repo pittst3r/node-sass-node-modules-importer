3ab2defe79c72d660f388925d0dad8f1372556db
Robbie Pitts <me@robbie-pitts.com>
2019-08-21 18:51:15 -0400

    Rewrite

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-var path = require('path');
-var findParentDir = require('find-parent-dir');
-var fs = require('fs');
+const path = require('path');
+const findParentDir = require('find-parent-dir');
+const fs = require('fs');
 
 function resolve(targetUrl, source) {
-  var packageRoot = findParentDir.sync(source, 'node_modules');
+  const packageRoot = findParentDir.sync(source, 'node_modules');
 
   if (!packageRoot) {
     return null;
   }
 
-  var filePath = path.resolve(packageRoot, 'node_modules', targetUrl);
-  var isPotentiallyDirectory = !path.extname(filePath);
+  const filePath = path.resolve(packageRoot, 'node_modules', targetUrl);
 
-  if (isPotentiallyDirectory) {
-    if (fs.existsSync(filePath + '.scss')) {
-      return filePath + '.scss';
-    }
-
-    if (fs.existsSync(filePath)) {
-      return path.resolve(filePath, 'index');
-    }
+  // @import '~my-module/my-file' where there is a '~/my-module/_my-file.scss'
+  const parts = path.parse(filePath);
+  const partialPath = path.join(parts.dir, '_' + parts.name);
+  if (fs.existsSync(partialPath + '.scss')) {
+    console.log(partialPath);
+    return partialPath;
   }
 
-  if (fs.existsSync(path.dirname(filePath))) {
+  // @import '~my-module/my-file' where there is a '~/my-module/my-file.scss'
+  if (fs.existsSync(filePath + '.scss')) {
+    return filePath;
+  }
+
+  // @import '~my-module/my-file' where there is a '~/my-module/my-file.css'
+  if (fs.existsSync(filePath + '.css')) {
+    return filePath;
+  }
+
+  // @import '~my-module/my-file.scss'
+  if (fs.existsSync(filePath)) {
     return filePath;
   }
 
   return resolve(targetUrl, path.dirname(packageRoot));
 }
 
-module.exports = function importer (url, prev, done) {
-  return (url[ 0 ] === '~') ? { file: resolve(url.substr(1), prev) } : null;
+module.exports = function importer(url, prev, done) {
+  return url[0] === '~' ? { file: resolve(url.substr(1), prev) } : null;
 };
--- index.test.js
+++ index.test.js
@@ -13,86 +13,12 @@ describe('Importer', function() {
 
   test('resolves to node_modules directory when first character is ~', function() {
     mockFs.existsSync.mockReturnValueOnce(false).mockReturnValue(true);
-    expect(importer('~my-module', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/index'
+    expect(importer('~my-module/foo', '')).toEqual({
+      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/foo'
     });
   });
 
   test('does nothing when the first character isnt a ~', function() {
     expect(importer('my-module', '')).toEqual(null);
-  });
-
-  test('recursively resolve url until package has not been found', function() {
-    var mockFsCheck = mockFs.existsSync,
-    mockParentDirFinder = mockFindParentDir.sync;
-
-    // url can not be resolved up to 10 level
-    for (var i = 0; i < 10; i++) {
-      mockFsCheck = mockFsCheck
-        .mockReturnValueOnce(false) // .scss import
-        .mockReturnValueOnce(false) // directory import
-        .mockReturnValueOnce(false); // file import
-
-      mockParentDirFinder = mockParentDirFinder.mockReturnValueOnce('MOCK_PARENT_DIR' + i);
-    }
-
-    // url finally found
-    mockFsCheck = mockFsCheck
-      .mockReturnValueOnce(false) // .scss import
-      .mockReturnValueOnce(false) // directory import
-      .mockReturnValueOnce(true); // file import
-    mockParentDirFinder = mockParentDirFinder.mockReturnValueOnce('MOCK_PARENT_DIR_final');
-
-    expect(importer('~my-module/test', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR_final/node_modules/my-module/test'
-    });
-
-    expect(mockParentDirFinder.mock.calls.length).toBe(11);
-    expect(mockFsCheck.mock.calls.length).toBe(33);
-  });
-
-  test('return null when package file can not be resolved', function() {
-    mockFs.existsSync.mockReturnValue(false);
-    mockFindParentDir.sync.mockReturnValue(null);
-    expect(importer('~my-module', '')).toEqual({ file: null });
-  });
-
-  test('should resolve extensions', function() {
-    mockFs.existsSync
-      .mockReturnValueOnce(false) // .scss import
-      .mockReturnValueOnce(true); // directory import
-
-    expect(importer('~my-module/test.scss', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test.scss'
-    });
-  });
-
-  test('should support file imports', function() {
-    mockFs.existsSync
-      .mockReturnValueOnce(false) // .scss import
-      .mockReturnValueOnce(false) // directory import
-      .mockReturnValueOnce(true); // file import
-
-    expect(importer('~my-module/test', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test'
-    });
-  });
-
-  test('should support directory imports', function() {
-    mockFs.existsSync
-      .mockReturnValueOnce(false) // .scss import
-      .mockReturnValueOnce(true); // directory import
-
-    expect(importer('~my-module/test', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test/index'
-    });
-  });
-
-  test('should support file imports minus extension', function() {
-    mockFs.existsSync.mockReturnValueOnce(true); // .scss import
-
-    expect(importer('~my-module/test', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test.scss'
-    });
   });
 });
